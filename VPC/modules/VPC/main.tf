011670bbf5a290e66c4078f99fab773d611ccaf6
Tushar <tp238015@gmail.com>
2026-01-19 10:06:11 +0530

    modify 7

--- VPC/modules/VPC/main.tf
+++ VPC/modules/VPC/main.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "public" {
   count                   = length(var.public_subnets)
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.public_subnets[count.index]
-  availability_zone       = var.azs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 
     tags = {
@@ -30,7 +30,7 @@ resource "aws_subnet" "private" {
   count             = length(var.private_subnets)
   vpc_id            = aws_vpc.this.id
   cidr_block        = var.private_subnets[count.index]
-  availability_zone = var.azs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
     tags = {
         Name = "privateSubnet${count.index + 1}"
     }
