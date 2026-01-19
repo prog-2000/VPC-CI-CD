7eb3401aa8499880a0c477f7b1aec9688f27eb51
Tushar <tp238015@gmail.com>
2026-01-19 09:50:33 +0530

    modify 5

--- VPC/provider.tf
+++ VPC/provider.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.28.0"
+      version = "~> 6.0"
     }
   }
+}
+
+provider "aws" {
+  region = var.aws_region
 }
