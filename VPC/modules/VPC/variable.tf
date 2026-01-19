db1a712951511d9d6aea11ee6de350fbaaffab94
Tushar <tp238015@gmail.com>
2026-01-19 11:05:17 +0530

    Revert "modify 8"
    This reverts commit 689efcd23a1f93b0eb0cc4e692819d46158817c3.

--- VPC/modules/VPC/variable.tf
+++ VPC/modules/VPC/variable.tf
@@ -1,10 +1,4 @@
 variable "vpc_cidr" { type = string }
-variable "public_subnets" { 
-    type = list(string) 
-default = [ "PublicSubnet1", "PublicSubnet2" ]
-}
-variable "private_subnets" { 
-    type = list(string) 
-    default = [ "PrivateSubnet1", "PrivateSubnet2" ]
-    }
+variable "public_subnets" { type = list(string) }
+variable "private_subnets" { type = list(string) }
 variable "azs" { type = list(string) }
