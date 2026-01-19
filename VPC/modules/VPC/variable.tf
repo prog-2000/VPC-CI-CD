689efcd23a1f93b0eb0cc4e692819d46158817c3
Tushar <tp238015@gmail.com>
2026-01-19 10:13:51 +0530

    modify 8

--- VPC/modules/VPC/variable.tf
+++ VPC/modules/VPC/variable.tf
@@ -1,4 +1,10 @@
 variable "vpc_cidr" { type = string }
-variable "public_subnets" { type = list(string) }
-variable "private_subnets" { type = list(string) }
+variable "public_subnets" { 
+    type = list(string) 
+default = [ "PublicSubnet1", "PublicSubnet2" ]
+}
+variable "private_subnets" { 
+    type = list(string) 
+    default = [ "PrivateSubnet1", "PrivateSubnet2" ]
+    }
 variable "azs" { type = list(string) }
